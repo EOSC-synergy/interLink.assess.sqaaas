49d30c26d9f4b034b3c58e0e435f74571215463b
orviz <orviz@ifca.unican.es>
2024-02-02 08:24:39 +0100

    Add JePL file structure

--- .sqa/script.wiggy-blue-gorilla.sh
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
-#
-# SPDX-License-Identifier: GPL-3.0-only
-
-(
-cd github.com/interTwin-eu/interLink &&
-    hadolint vendor/golang.org/x/net/http2/Dockerfile vendor/github.com/CARV-ICS-FORTH/knoc/Dockerfile --failure-threshold error
-)
